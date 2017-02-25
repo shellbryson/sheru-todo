2dac78aec18c06f5dc2dc677358b9eb27c22b545
Shell Bryson <shell@shellbryson.com>
2017-02-25 13:15:23 +0000

    Adds basic functionality,

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,52 @@
-alert("hello world");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create component
+var TitleComponent = React.createClass({
+  render: function() {
+    var _props = this.props.ctx;
+    return(
+      <div>
+        <h1>{_props.heading}</h1>
+        <h2>{_props.subheading}</h2>
+      </div>
+    );
+  }
+});
+
+var title = { heading: 'Todo Experiment', subheading: "An experiment with React" };
+
+ReactDOM.render(<TitleComponent ctx={title} />, document.getElementById('title'));
+
+// Create component
+var ToDo = React.createClass({
+  getInitialState: function() {
+    return {
+      todos:['wash up', 'eat some cheese', 'take a shower'],
+      age: 30
+    }
+  },
+  render: function() {
+
+    // We bind 'this' to the setTimeout so that setState can find it
+    var ager = setTimeout(function(){
+      this.setState({
+        age: 35
+      })
+    }.bind(this), 5000);
+
+    return(
+      <div id="todo list">
+      <p>This is a todo list</p>
+      <p>{this.state.age}</p>
+      <ul>
+        <li>{this.state.todos[0]}</li>
+        <li>{this.state.todos[1]}</li>
+        <li>{this.state.todos[2]}</li>
+      </ul>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<ToDo ctx={title} />, document.getElementById('wrapper'));
